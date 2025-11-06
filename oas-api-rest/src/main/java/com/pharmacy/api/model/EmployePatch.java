73acd9d71be98c11523642ac0d59d551cfbbbb5f
Erik-Est-Vaz <l22140765@queretaro.tecnm.mx>
2025-11-06 08:34:25 -0600

    Update EmployePatch.java

--- oas-api-rest/src/main/java/com/pharmacy/api/model/EmployePatch.java
+++ oas-api-rest/src/main/java/com/pharmacy/api/model/EmployePatch.java
@@ -36,6 +36,226 @@ import org.hibernate.validator.constraints.*;
  */
 @JsonPropertyOrder({
   EmployePatch.JSON_PROPERTY_NAME,
+  EmployePatch.JSON_PROPERTY_LAST_NAME,
+  EmployePatch.JSON_PROPERTY_PHONE
+})
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-11-04T13:50:12.904429500-06:00[America/Mexico_City]")
+public class EmployePatch {
+  public static final String JSON_PROPERTY_NAME = "name";
+  private String name;
+
+  public static final String JSON_PROPERTY_LAST_NAME = "lastName";
+  private String lastName;
+
+  public static final String JSON_PROPERTY_PHONE = "phone";
+  private String phone;
+
+  public EmployePatch() { 
+  }
+
+  public EmployePatch name(String name) {
+    this.name = name;
+    return this;
+  }
+
+   /**
+   * Get name
+   * @return name
+  **/
+  @jakarta.annotation.Nullable
+ @Size(min=3,max=100)
+  @JsonProperty(JSON_PROPERTY_NAME)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+
+  public String getName() {
+    return name;
+  }
+
+
+  @JsonProperty(JSON_PROPERTY_NAME)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public EmployePatch lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+   /**
+   * Get lastName
+   * @return lastName
+  **/
+  @jakarta.annotation.Nullable
+ @Size(min=3,max=100)
+  @JsonProperty(JSON_PROPERTY_LAST_NAME)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+
+  public String getLastName() {
+    return lastName;
+  }
+
+
+  @JsonProperty(JSON_PROPERTY_LAST_NAME)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public EmployePatch phone(String phone) {
+    this.phone = phone;
+    return this;
+  }
+
+    /**
+    * Get phone
+    * @return phone
+    **/
+  @jakarta.annotation.Nullable
+  @Size(min=10,max=20) // Uso las validaciones de tu esquema 'employe'
+  @JsonProperty(JSON_PROPERTY_PHONE)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+
+  public String getPhone() {
+    return phone;
+  }
+
+
+  @JsonProperty(JSON_PROPERTY_PHONE)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+
+  /**
+   * Return true if this employePatch object is equal to o.
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    EmployePatch employePatch = (EmployePatch) o;
+    return Objects.equals(this.name, employePatch.name) &&
+        Objects.equals(this.lastName, employePatch.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, lastName);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class EmployePatch {\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
+  /**
+   * Convert the instance into URL query string.
+   *
+   * @return URL query string
+   */
+  public String toUrlQueryString() {
+    return toUrlQueryString(null);
+  }
+
+  /**
+   * Convert the instance into URL query string.
+   *
+   * @param prefix prefix of the query string
+   * @return URL query string
+   */
+  public String toUrlQueryString(String prefix) {
+    String suffix = "";
+    String containerSuffix = "";
+    String containerPrefix = "";
+    if (prefix == null) {
+      // style=form, explode=true, e.g. /pet?name=cat&type=manx
+      prefix = "";
+    } else {
+      // deepObject style e.g. /pet?id[name]=cat&id[type]=manx
+      prefix = prefix + "[";
+      suffix = "]";
+      containerSuffix = "]";
+      containerPrefix = "[";
+    }
+
+    StringJoiner joiner = new StringJoiner("&");
+
+    // add `name` to the URL query string
+    if (getName() != null) {
+      joiner.add(String.format("%sname%s=%s", prefix, suffix, URLEncoder.encode(String.valueOf(getName()), StandardCharsets.UTF_8).replaceAll("\\+", "%20")));
+    }
+
+    // add `lastName` to the URL query string
+    if (getLastName() != null) {
+      joiner.add(String.format("%slastName%s=%s", prefix, suffix, URLEncoder.encode(String.valueOf(getLastName()), StandardCharsets.UTF_8).replaceAll("\\+", "%20")));
+    }
+
+    return joiner.toString();
+  }
+}
+
+/*
+ * pharmacy
+ * API para administrar una farmacia
+ *
+ * The version of the OpenAPI document: 1.3.0
+ * Contact: l22140765@queretaro.tecnm.mx
+ *
+ * NOTE: This class is auto generated by OpenAPI Generator (https://openapi-generator.tech).
+ * https://openapi-generator.tech
+ * Do not edit the class manually.
+ */
+
+
+package com.pharmacy.api.model;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.StringJoiner;
+import java.util.Objects;
+import java.util.Map;
+import java.util.HashMap;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Arrays;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.validation.constraints.*;
+import jakarta.validation.Valid;
+import org.hibernate.validator.constraints.*;
+
+
+/**
+ * EmployePatch
+ */
+@JsonPropertyOrder({
+  EmployePatch.JSON_PROPERTY_NAME,
   EmployePatch.JSON_PROPERTY_LAST_NAME
 })
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-11-04T13:50:12.904429500-06:00[America/Mexico_City]")
