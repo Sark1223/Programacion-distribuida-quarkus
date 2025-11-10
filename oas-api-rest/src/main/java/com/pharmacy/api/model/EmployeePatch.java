30d4b78dc813ea1b91aa561e0ba9e85d3880cbaa
Erik-Est-Vaz <l22140765@queretaro.tecnm.mx>
2025-11-10 08:22:43 -0600

    Update EmployeePatch.java

--- oas-api-rest/src/main/java/com/pharmacy/api/model/EmployeePatch.java
+++ oas-api-rest/src/main/java/com/pharmacy/api/model/EmployeePatch.java
@@ -32,15 +32,15 @@ import org.hibernate.validator.constraints.*;
 
 
 /**
- * EmployeePatch
+ * EmployePatch
  */
 @JsonPropertyOrder({
-  EmployeePatch.JSON_PROPERTY_NAME,
-  EmployeePatch.JSON_PROPERTY_LAST_NAME,
-  EmployeePatch.JSON_PROPERTY_PHONE
+  EmployePatch.JSON_PROPERTY_NAME,
+  EmployePatch.JSON_PROPERTY_LAST_NAME,
+  EmployePatch.JSON_PROPERTY_PHONE
 })
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-11-06T18:47:26.075651800-06:00[America/Mexico_City]")
-public class EmployeePatch {
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-11-04T13:50:12.904429500-06:00[America/Mexico_City]")
+public class EmployePatch {
   public static final String JSON_PROPERTY_NAME = "name";
   private String name;
 
@@ -50,10 +50,10 @@ public class EmployeePatch {
   public static final String JSON_PROPERTY_PHONE = "phone";
   private String phone;
 
-  public EmployeePatch() { 
+  public EmployePatch() { 
   }
 
-  public EmployeePatch name(String name) {
+  public EmployePatch name(String name) {
     this.name = name;
     return this;
   }
@@ -79,7 +79,7 @@ public class EmployeePatch {
   }
 
 
-  public EmployeePatch lastName(String lastName) {
+  public EmployePatch lastName(String lastName) {
     this.lastName = lastName;
     return this;
   }
@@ -104,18 +104,17 @@ public class EmployeePatch {
     this.lastName = lastName;
   }
 
-
-  public EmployeePatch phone(String phone) {
+  public EmployePatch phone(String phone) {
     this.phone = phone;
     return this;
   }
 
-   /**
-   * Get phone
-   * @return phone
-  **/
+    /**
+    * Get phone
+    * @return phone
+    **/
   @jakarta.annotation.Nullable
- @Size(min=10,max=20)
+  @Size(min=10,max=20) // Uso las validaciones de tu esquema 'employe'
   @JsonProperty(JSON_PROPERTY_PHONE)
   @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
 
@@ -132,7 +131,7 @@ public class EmployeePatch {
 
 
   /**
-   * Return true if this employeePatch object is equal to o.
+   * Return true if this employePatch object is equal to o.
    */
   @Override
   public boolean equals(Object o) {
@@ -142,24 +141,22 @@ public class EmployeePatch {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    EmployeePatch employeePatch = (EmployeePatch) o;
-    return Objects.equals(this.name, employeePatch.name) &&
-        Objects.equals(this.lastName, employeePatch.lastName) &&
-        Objects.equals(this.phone, employeePatch.phone);
+    EmployePatch employePatch = (EmployePatch) o;
+    return Objects.equals(this.name, employePatch.name) &&
+        Objects.equals(this.lastName, employePatch.lastName);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name, lastName, phone);
+    return Objects.hash(name, lastName);
   }
 
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    sb.append("class EmployeePatch {\n");
+    sb.append("class EmployePatch {\n");
     sb.append("    name: ").append(toIndentedString(name)).append("\n");
     sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
-    sb.append("    phone: ").append(toIndentedString(phone)).append("\n");
     sb.append("}");
     return sb.toString();
   }
@@ -215,11 +212,6 @@ public class EmployeePatch {
     // add `lastName` to the URL query string
     if (getLastName() != null) {
       joiner.add(String.format("%slastName%s=%s", prefix, suffix, URLEncoder.encode(String.valueOf(getLastName()), StandardCharsets.UTF_8).replaceAll("\\+", "%20")));
-    }
-
-    // add `phone` to the URL query string
-    if (getPhone() != null) {
-      joiner.add(String.format("%sphone%s=%s", prefix, suffix, URLEncoder.encode(String.valueOf(getPhone()), StandardCharsets.UTF_8).replaceAll("\\+", "%20")));
     }
 
     return joiner.toString();
