c4586d8ab1f5fc579d4a790638f061f07710a743
Erik-Est-Vaz <l22140765@queretaro.tecnm.mx>
2025-11-11 00:23:00 -0600

    Update EmployeesResource.java

--- oas-api-rest/src/main/java/com/pharmacy/api/resource/EmployeesResource.java
+++ oas-api-rest/src/main/java/com/pharmacy/api/resource/EmployeesResource.java
@@ -1,14 +1,14 @@
 package com.pharmacy.api.resource;
 
 import com.pharmacy.api.commons.AlreadyExistsException;
-import com.pharmacy.api.model.Employe;
-import com.pharmacy.api.model.EmployePatch;
+import com.pharmacy.api.model.Employee;
+import com.pharmacy.api.model.EmployeePatch;
 import com.pharmacy.api.model.Error400BadRequest;
 import com.pharmacy.api.model.Error404NotFound;
 import com.pharmacy.api.model.Product;
 import com.pharmacy.api.model.Success;
 import com.pharmacy.api.model.SuccessWithData;
-import com.pharmacy.api.service.EmployesService;
+import com.pharmacy.api.service.EmployeesService;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
@@ -25,152 +25,153 @@ import java.util.Map;
 @Path("/api/v1")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class EmployesResource {
+public class EmployeesResource {
 
     @Inject
-    EmployesService employeService;
+    EmployeesService employeeService;
 
 
-    // 1. --- GET /employes : Obtener todos los empleados (200 OK) ---
+    // 1. --- GET /employees : Obtener todos los empleados (200 OK) ---
     @GET
-    @Path("/employes")
-    public Response getEmployes() {
+    @Path("/employees")
+    public Response getEmployees() {
         System.out.println("Resource - Solicitud GET: obtener todos los empleados.");
-        List<Employe> employes = employeService.getEmployes();
+        List<Employee> employees = employeeService.getEmployees();
         
         SuccessWithData response = new SuccessWithData();
         response.setCode("EMPLOYEES_RETRIEVED");
         response.setMessage("Lista de empleados obtenida exitosamente");
-        response.setData(employes);
+        response.setData(employees);
     
         return Response.ok(response).build();
     }
     
-    // 2. --- GET /employes/{idEmploye} : Obtener un empleado por ID (200 OK / 404 NOT_FOUND) ---
+    // 2. --- GET /employees/{idEmployee} : Obtener un empleado por ID (200 OK / 404 NOT_FOUND) ---
     @GET
-    @Path("/employes/{id}")
-    public Response getEmploye(@PathParam("id") Integer idEmploye) {
-        System.out.println("Resource - Solicitud GET: obtener empleado por ID: " + idEmploye);
-            Employe employe = employeService.getEmploye(idEmploye);
+    @Path("/employees/{id}")
+    public Response getEmployee(@PathParam("id") Integer idEmployee) {
+        System.out.println("Resource - Solicitud GET: obtener empleado por ID: " + idEmployee);
+            Employee employee = employeeService.getEmployee(idEmployee);
             
-            if(employe == null) {
+            if(employee == null) {
                 Error404NotFound errorResponse = new Error404NotFound();
-                errorResponse.setCode("PRODUCT_NOT_FOUND");
-                errorResponse.setMessage("No se encontró el producto con el ID proporcionado.");
-                errorResponse.putDetailsItem("Invalid",idEmploye);
-                return Response.status(Response.Status.BAD_REQUEST).entity(errorResponse).build();
+                errorResponse.setCode("EMPLOYEE_NOT_FOUND");
+                errorResponse.setMessage("No se encontró el empleado con el ID proporcionado.");
+                errorResponse.putDetailsItem("Invalid",idEmployee);
+                return Response.status(Response.Status.NOT_FOUND).entity(errorResponse).build();
             }
 
             Success response = new Success();
-            response.setCode("EMPLOYE_RETRIEVED");
+            response.setCode("EMPLOYEE_RETRIEVED");
             response.setMessage("Información del empleado obtenida exitosamente");
-            response.setData(employe);
+            response.setData(employee);
             
             return Response.ok(response).status(Status.OK).build();
             
     }
 
-    // 3. --- POST /employes : Crear nuevo empleado (201 CREATED / 400 BAD_REQUEST) ---
+    // 3. --- POST /employees : Crear nuevo empleado (201 CREATED / 400 BAD_REQUEST) ---
     @POST
-    @Path("/employes")
-    public Response createEmploye(@Valid Employe employeRequest) {
+    @Path("/employees")
+    public Response createEmploye(@Valid Employee employeeRequest) {
         System.out.println("Resource - Solicitud POST: crear nuevo empleado.");
-            Employe savedEmploye = employeService.createEmploye(employeRequest);
+            Employee savedEmployee = employeeService.createEmployee(employeeRequest);
 
             Success response = new Success();
-            response.setCode("EMPLOYE_CREATED");
+            response.setCode("EMPLOYEE_CREATED");
             response.setMessage("Empleado registrado exitosamente");
-            response.setData(savedEmploye);
+            response.setData(savedEmployee);
     
         return Response.status(Response.Status.CREATED).entity(response).build();
  
     }
 
-    // 4. --- PUT /employes/{idEmploye} : Actualizar todos los datos (201 CREATED / 404 NOT_FOUND / 400 BAD_REQUEST) ---
+    // 4. --- PUT /employees/{idEmployee} : Actualizar todos los datos (201 CREATED / 404 NOT_FOUND / 400 BAD_REQUEST) ---
     @PUT
-    @Path("/employes/{id}")
-    public Response updateEmploye(@PathParam("id") Integer idEmploye, @Valid Employe employeRequest) {
-        System.out.println("Resource - Solicitud PUT: actualizar empleado ID: " + idEmploye);
+    @Path("/employees/{id}")
+    public Response updateEmployee(@PathParam("id") Integer idEmployee, @Valid Employee employeeRequest) {
+        System.out.println("Resource - Solicitud PUT: actualizar empleado ID: " + idEmployee);
         
-        if (idEmploye == null || idEmploye < 1 || idEmploye > 2147483647) {
+        if (idEmployee < 1 || idEmployee > 2147483647) {
             Error400BadRequest errorResponse = new Error400BadRequest();
             errorResponse.setCode("INVALID_EMPLOYEE_ID");
             errorResponse.setMessage("El ID del empleado es inválido. Debe estar entre 1 y 2147483647.");
-            errorResponse.putDetailsItem("invalidId", idEmploye);
+            errorResponse.putDetailsItem("invalidId", idEmployee);
             return Response.status(Response.Status.BAD_REQUEST).entity(errorResponse).build();
         }
 
-       Employe updateEmploye = employeService.updateEmploye(idEmploye, employeRequest);
+       Employee updateEmployee = employeeService.updateEmployee(idEmployee, employeeRequest);
 
-        if (updateEmploye == null) {
+        if (idEmployee == null || updateEmployee == null) {
             Error404NotFound errorResponse = new Error404NotFound();
             errorResponse.setCode("INVALID_EMPLOYEE_ID");
             errorResponse.setMessage("El ID del empleado no esta registrado.");
-            errorResponse.putDetailsItem("invalidId", idEmploye);
+            errorResponse.putDetailsItem("invalidId", idEmployee);
             return Response.status(Response.Status.NOT_FOUND).entity(errorResponse).build();
         }
 
         Success response = new Success();
-        response.setCode("PRODUCT_UPDATED");
-        response.setMessage("Producto actualizado exitosamente");
-        response.setData(updateEmploye);
+        response.setCode("EMPLOYEE_UPDATED");
+        response.setMessage("Empleado actualizado exitosamente");
+        response.setData(updateEmployee);
         
         return Response.ok(response).build();
 
     }
 
-    // 5. --- PATCH /employes/{idEmploye} : Actualización parcial (200 OK / 404 NOT_FOUND) ---
+    // 5. --- PATCH /employees/{idEmployee} : Actualización parcial (200 OK / 404 NOT_FOUND) ---
     @PATCH
-    @Path("/employes/{id}")
-    public Response patchEmploye(@PathParam("id") Integer idEmploye, @Valid EmployePatch employePatch) {
-        System.out.println("Resource - Solicitud PATCH: actualizar parcialmente empleado ID: " + idEmploye);
+    @Path("/employees/{id}")
+    public Response patchEmploye(@PathParam("id") Integer idEmployee, @Valid EmployeePatch employeePatch) {
+        System.out.println("Resource - Solicitud PATCH: actualizar parcialmente empleado ID: " + idEmployee);
         
-        if (idEmploye == null || idEmploye < 1 || idEmploye > 2147483647) {
+        if (idEmployee < 1 || idEmployee > 2147483647) {
             Error400BadRequest errorResponse = new Error400BadRequest();
             errorResponse.setCode("INVALID_EMPLOYEE_ID");
             errorResponse.setMessage("El ID del empleado es inválido. Debe estar entre 1 y 2147483647.");
-            errorResponse.putDetailsItem("invalidId", idEmploye);
+            errorResponse.putDetailsItem("invalidId", idEmployee);
             return Response.status(Response.Status.BAD_REQUEST).entity(errorResponse).build();
         }
 
-       Employe patchedEmploye = employeService.patchEmploye(idEmploye, employePatch);
+       Employee patchedEmployee = employeeService.patchEmployee(idEmployee, employeePatch);
 
-        if (patchedEmploye == null) {
+        if (idEmployee == null || patchedEmployee == null) {
             Error404NotFound errorResponse = new Error404NotFound();
             errorResponse.setCode("INVALID_EMPLOYEE_ID");
             errorResponse.setMessage("El ID del empleado no esta registrado.");
-            errorResponse.putDetailsItem("invalidId", idEmploye);
+            errorResponse.putDetailsItem("invalidId", idEmployee);
             return Response.status(Response.Status.NOT_FOUND).entity(errorResponse).build();
         }
 
         Success response = new Success();
-        response.setCode("PRODUCT_PATCHED");
-        response.setMessage("Producto actualizado parcialmente exitosamente");
-        response.setData(patchedEmploye);
+        response.setCode("EMPLOYEE_PATCHED");
+        response.setMessage("Empleado actualizado parcialmente exitosamente");
+        response.setData(patchedEmployee);
         
         return Response.ok(response).build();
     }
     
-    // 6. --- DELETE /employes/{idEmploye} : Eliminar empleado (200 OK / 404 NOT_FOUND) ---
+    // 6. --- DELETE /employees/{idEmployee} : Eliminar empleado (200 OK / 404 NOT_FOUND) ---
     @DELETE
-    @Path("/employes/{id}")
-    public Response deleteEmploye(@PathParam("id") Integer idEmploye) {
-        System.out.println("Resource - Solicitud DELETE: eliminar empleado ID: " + idEmploye);
+    @Path("/employees/{id}")
+    public Response deleteEmployee(@PathParam("id") Integer idEmployee) {
+        System.out.println("Resource - Solicitud DELETE: eliminar empleado ID: " + idEmployee);
 
-        if (idEmploye == null || idEmploye < 1 || idEmploye > 2147483647) {
+        if (idEmployee < 1 || idEmployee > 2147483647) {
             Error400BadRequest errorResponse = new Error400BadRequest();
             errorResponse.setCode("INVALID_EMPLOYEE_ID");
             errorResponse.setMessage("El ID del empleado es inválido. Debe estar entre 1 y 2147483647.");
-            errorResponse.putDetailsItem("invalidId", idEmploye);
+            errorResponse.putDetailsItem("invalidId", idEmployee);
             return Response.status(Response.Status.BAD_REQUEST).entity(errorResponse).build();
         }
-            
-        String deletionResultEmploye = employeService.deleteEmploye(idEmploye);
-                
+
+                   
+        String deletionResultEmployee = employeeService.deleteEmployee(idEmployee);
+   
         Success response = new Success();
-        response.setCode("EMPLOYE_DELETED");
-        response.setMessage("Eliminado eliminado exitosamente");
-        response.setData(deletionResultEmploye);
+        response.setCode("EMPLOYEE_DELETED");
+        response.setMessage("Eliminado empleado exitosamente");
+        response.setData(deletionResultEmployee);
     
         return Response.ok(response).build();
     }
